339565db97500721907211abc051270c8bcc3cb7
GET Academy <terje@kolderup.net>
2021-08-31 12:56:23 +0200

    Initial commit

---
+++ NumberList/List.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberList
+{
+    class List<T>
+    {
+        T[] _numbers;
+        int _count;
+
+        public List()
+        {
+            _numbers = new T[4];
+            _count = 0;
+        }
+
+        public void Add(T value )
+        {
+            if (_count >= _numbers.Length)
+            {
+                var numbersTmp = _numbers;
+                _numbers = new T[_numbers.Length * 2];
+                Array.Copy(numbersTmp, _numbers, numbersTmp.Length);
+                Console.WriteLine("Doblet kapasitet til " + _numbers.Length);
+            }
+            _numbers[_count] = value;
+            _count++;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(string.Join(", ", _numbers));
+        }
+    }
+}
---
+++ NumberList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NumberList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Skriv inn et navn: ");
+                var name = Console.ReadLine();
+                if (name =="") return;
+                list.Add(name);
+                list.Show();
+            }
+        }
+
+        static void MainInt(string[] args)
+        {
+            var list = new List<int>();
+            var list2 = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Skriv inn et tall: ");
+                var numberStr = Console.ReadLine();
+                var number = Convert.ToInt32(numberStr);
+                if (number == 0) return;
+                var correctList = number > 100 ? list : list2;
+                correctList.Add(number);
+                correctList.Show();
+            }
+        }
+    }
+}
